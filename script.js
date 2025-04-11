e1a16ccb8d2acbb97498371ccdcf2c7043d56ec7
LaurentD974 <laurentdevaud974@gmail.com>
2025-04-11 13:11:49 +0400

    Version 2 terminée

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-//1. Récupère le btn par son identifiant
-const btn = document.getElementById("btnApi");
-const imgContainer = document.getElementById("img-container");
+const nom=document.getElementById("nom");
+const prenom=document.getElementById("prenom");
+const genre=document.getElementById("genre");
+const telephone=document.getElementById("telephone");
+const email=document.getElementById("email");
+const photo=document.getElementById("photo");
 
-//Afficher l'élément dans la console.
-// console.log(btn);
-
-//2. Ajoute l'evenement sur le btn
-btn.addEventListener("click", fetchRandomUsr);
-
-//3 . Fonction qui se déclenche au clic
-function updateImgContainer(){
-    imgContainer.innerHTML = "<a href='https://google.fr'> aller sur google </a>"
-}
-
-//4. Fetch url image du chien 
+//4. Fetch url image de l'utilisateur
 async function fetchRandomUsr(){
     const request = await fetch("https://randomuser.me/api/");
     const response = await request.json();
-    const urlImage = response.message;   
-   
-    // Extraire la race de l'URL de l'image
-    const breed = urlImage.split('/')[4].replace('-', ' '); // Remplace le tiret par un espace pour une meilleure présentation
+    const resultat = response.results[0]
 
-    // Afficher l'image et la race
-    imgContainer.innerHTML = `
-        <img src="${urlImage}" alt="user"/>
-        <p>nom : ${breed}</p>
-    `;
+    nom.innerHTML = resultat.name.last
+    prenom.innerHTML = resultat.name.first
+    genre.innerHTML = resultat.name.title
+    telephone.innerHTML = resultat.phone
+    email.innerHTML = resultat.email
+    photo.src = resultat.picture.large
+    
+console.log(resultat.name.last)
+console.log(resultat.name.first)
+console.log(resultat.name.title)
+console.log(resultat.phone)
+console.log(resultat.email)
+console.log(resultat.picture.medium)
+
 }
 
+fetchRandomUsr()
 
 
   
