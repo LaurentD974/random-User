f475a3e8ddb75cfe9551ad0fb820c08cc91a886d
LaurentD974 <laurentdevaud974@gmail.com>
2025-04-11 15:28:59 +0400

    Carte de visite * 20

--- script.js
+++ script.js
@@ -7,17 +7,35 @@ const photo=document.getElementById("photo");
 
 //4. Fetch url image de l'utilisateur
 async function fetchRandomUsr(){
-    const request = await fetch("https://randomuser.me/api/");
+    const request = await fetch("https://randomuser.me/api/?results=20");
     const response = await request.json();
-    const resultat = response.results[0]
-
-    nom.innerHTML = resultat.name.last
-    prenom.innerHTML = resultat.name.first
-    genre.innerHTML = resultat.name.title
-    telephone.innerHTML = resultat.phone
-    email.innerHTML = resultat.email
-    photo.src = resultat.picture.large
     
+    
+for (let i = 0; i < response.results.length; i++) {
+
+
+        const resultat =response.results[i];
+        const cardecontainer=document.getElementById("cardecontainer");
+        console.log(resultat)
+
+        cardecontainer.innerHTML += `<div id="section1">
+      <div class="main-left">
+        <img id="photo" src="${resultat.picture.large}" />
+      </div>
+      <div class="main-right">
+        <ul>
+          <li id="nom">${resultat.name.last}</li>
+          <li id="prenom">${resultat.name.first}</li>
+          <li id="genre">${resultat.name.title}</li>
+          <li id="telephone">${resultat.phone}</li>
+          <li id="email">${resultat.email}</li>
+        </ul>
+      </div>
+     
+    </div>
+     <h1>CARTE DE VISITE</h1>`
+   
+   
 console.log(resultat.name.last)
 console.log(resultat.name.first)
 console.log(resultat.name.title)
@@ -26,10 +44,6 @@ console.log(resultat.email)
 console.log(resultat.picture.medium)
 
 }
-
+};
 fetchRandomUsr()
 
-
-  
-
-  
